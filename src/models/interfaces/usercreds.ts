8de47401ad8728738af544322bb5e3bd76e6c36c
elen1k <elenikali@hotmail.com>
2017-07-23 15:34:28 +0300

    Add files via upload

---
+++ src/models/interfaces/usercreds.ts
@@ -0,0 +1,15 @@
+export interface usercreds {
+        id: string;
+        email: string;
+        score: number;
+        chapter1: number;
+        chapter2: number;
+        chapter3: number;
+        chapter4: number;
+        chapter5: number;
+        chapter6: number;
+        chapter7: number;
+        easy: number;
+        medium: number;
+        hard: number;
+      }
